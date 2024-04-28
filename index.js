70115033711600bf1931f1a0f65ad43f3f6c77e7
JoelhansenMN <joelhansen22@gmail.com>
2024-04-27 21:31:35 -0500

    added character limit to index.js

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
 const inquirer = require("inquirer");
 const {Circle, Square, Triangle} = require("./lib/shape");
 const fs = require("fs");
+const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt'); //add on in the inquirer package to set max length of input
 
-// class SVG{
-//     constructor(){
-//   this.version = "1.1";
-//   this.width = "300";
-//   this.height = "200";
-//   this.xmlns = "https://www.w3.org/2000/svg";
-//     }
-    
-//     render(){
-//       return `<svg version="${this.version}" width="${this.width}" height="${this.height}" xmlns="${this.xmlns}">`
-//     }
-//   }
+
+inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt)
 
 const questions = () => inquirer.prompt([
 
   {
-  type: "input",
+  type: "maxlength-input", //sets max length
   name: "text",
-  message: "Please enter up to 3 characters:"
+  message: "Please enter up to 3 characters:",
+  maxLength: 3  //max length set to three
   },
 
   {
@@ -32,7 +24,7 @@ const questions = () => inquirer.prompt([
 
   {
     type: "input",
-    name: "shapeColor",
+    name: "shapeColor",  //sets background of image color
     message: "Please enter the shape-color you would like for your SVG:"
   },
 
@@ -45,7 +37,7 @@ const questions = () => inquirer.prompt([
 ])
 
 .then(({text, shape, shapeColor, textColor}) => {
-  //const SVG = new SVG()
+ 
     switch(shape){
       case 'Circle':
         shape = new Circle(shapeColor)
